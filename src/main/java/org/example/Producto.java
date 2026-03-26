09e31ced3a4fb413714207bd0c9811d688bca57b
roquevisciarelli <rvisciarelli2@gmail.com>
2026-03-26 10:45:52 -0300

    mensaje

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 public class Producto {
     private String nombre;
     private double precio;
@@ -14,4 +16,36 @@ public class Producto {
         this.stock = stock;
     }
 //Generar Getters, Setters y metodo toString.
+
+
+    public static List<Producto> cargarProductos() {
+        return List.of(
+                new Producto("Laptop", 1500, "Electrónica", 5),
+                new Producto("Smartphone", 800, "Electrónica", 10),
+                new Producto("Televisor", 1200, "Electrónica", 3),
+                new Producto("Heladera", 2000, "Hogar", 2),
+                new Producto("Microondas", 500, "Hogar", 8),
+                new Producto("Silla", 150, "Muebles", 12),
+                new Producto("Mesa", 300, "Muebles", 7),
+                new Producto("Zapatillas", 100, "Deportes", 15),
+                new Producto("Pelota", 50, "Deportes", 20),
+                new Producto("Bicicleta", 500, "Deportes", 5),
+                new Producto("Libro", 30, "Librería", 50),
+                new Producto("Cuaderno", 10, "Librería", 100),
+                new Producto("Lámpara", 80, "Hogar", 30),
+                new Producto("Cafetera", 250, "Hogar", 6),
+                new Producto("Auriculares", 120, "Electrónica", 14),
+                new Producto("Teclado", 90, "Electrónica", 9),
+                new Producto("Mouse", 60, "Electrónica", 18),
+                new Producto("Monitor", 700, "Electrónica", 4),
+                new Producto("Cama", 800, "Muebles", 2),
+                new Producto("Sofá", 1000, "Muebles", 3),
+                new Producto("Espejo", 120, "Hogar", 12),
+                new Producto("Ventilador", 150, "Hogar", 7),
+                new Producto("Patines", 180, "Deportes", 5),
+                new Producto("Raqueta", 220, "Deportes", 6),
+                new Producto("Taza", 15, "Hogar", 40)
+        );
+    }
+
 }
