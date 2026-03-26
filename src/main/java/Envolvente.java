c9becf3ccc42ea2416c1903506cc2e9e4ecfefcb
mateo <mateo.asaro123@gmail.com>
2026-03-26 11:01:45 -0300

    quinoto pedazo de chupapija

---
+++ src/main/java/Envolvente.java
@@ -0,0 +1,11 @@
+import org.example.Producto;
+
+import java.util.ArrayList;
+
+public class Envolvente {
+    private ArrayList<Producto> productos;
+
+    public Envolvente(ArrayList<Producto> electronica) {
+        this.productos = new ArrayList<>();
+    }
+}
