ed974857afacd7637aa733abc2643036331d122a
Joaquin Iriarte <joaquiniriarte978@gmail.com>
2026-03-26 10:54:52 -0300

    primer paso

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -15,8 +15,48 @@ public class Producto {
         this.categoria = categoria;
         this.stock = stock;
     }
-//Generar Getters, Setters y metodo toString.
 
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "categoria='" + categoria + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
 
     public static List<Producto> cargarProductos() {
         return List.of(
