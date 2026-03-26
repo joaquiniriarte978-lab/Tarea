e7807633195540680d48a7ea64789018abc77acf
mateo <mateo.asaro123@gmail.com>
2026-03-26 10:30:30 -0300

    clase producto

---
+++ src/main/java/org/example/Producto.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+    private String categoria;
+    private int stock;
+    // Constructor, Getters y Setters
+    public Producto(String nombre, double precio, String categoria, int
+            stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;
+    }
+//Generar Getters, Setters y metodo toString.
+}
