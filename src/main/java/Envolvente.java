7370255ccded6bef25364c22394427123b0e25bb
roquevisciarelli <rvisciarelli2@gmail.com>
2026-03-26 11:22:35 -0300

    punto1

--- src/main/java/Envolvente.java
+++ src/main/java/Envolvente.java
@@ -1,7 +1,9 @@
 import org.example.Producto;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Envolvente {
     private List<Producto> productos;
@@ -9,6 +11,18 @@ public class Envolvente {
     public Envolvente() {
         this.productos = Producto.cargarProductos();
     }
+
+
+    public List<String> obtenerElectronicaCara() {
+        return productos.stream()
+                .filter(p -> "Electronica".equalsIgnoreCase(p.getCategoria()) )
+                .filter(p -> p.getPrecio() > 1000 )
+                .sorted(Comparator.comparingDouble(Producto::getPrecio).reversed())
+                .map(p -> p.getNombre() + "- $" + p.getPrecio())
+                .collect(Collectors.toList());
+
+    }
+
     public void masCaroXcategoria(){
 
     }
